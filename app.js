172c513e6c033389ea29f3cb1fea40eb851fde42
ibrahimnavadia <faisalnavadia3@gmail.com>
2025-12-27 03:20:43 +0500

    updated version

--- app.js
+++ app.js
@@ -19,3 +19,30 @@ for(let i = 0; i < words.length; i++){
 
 console.log("vowels are:" + foundVowels);
 console.log("totalVowels:" + count);
+
+
+let text = "lorem ispmup dolor sit amet consettetusC lorem ispmup dolor sit amet consettetusC lorem ispmup dolor sit amet consettetusC lorem ispmup dolor sit amet consettetusC lorem ispmup dolor sit amet consettetusC lorem ispmup dolor sit amet consettetusC lorem ispmup dolor sit amet consettetusC lorem ispmup dolor sit amet consettetusC lorem ispmup dolor sit amet consettetusC"
+//  console.log(text)
+
+ for(let i = 0; i < text.lenght; i++){
+  const targetWord = "lorem ispump"
+  const newWord = "hello world"
+  const targetWordlenght = targetWord.lenght;
+
+const startIndexOfTargetElement = text.indexOf(targetWord);
+
+const endIndexofTargetElement = startIndexOfTargetElement + targetWordlenght - 1; 
+
+if(startIndexOfTargetElement != 1){
+  const earlierPart = text.slice(0, startIndexOfTargetElement);
+
+  const afterPart = text.slice(endIndexofTargetElement + 1);
+
+  text = earlierPart + newWord + afterPart;
+}
+
+ }
+
+   text  = text.replace("lorem ispmup", "hello world")
+   text = text.replaceAll("lorem ispmup", "hello world")
+   console.log(text)
